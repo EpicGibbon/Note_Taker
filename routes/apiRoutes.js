d0f9fa88088d55d1b1506fdbe4ba33cd23a7f2c0
EpicGibbon <michant1997@gmail.com>
2020-07-11 14:42:51 -0400

    have initial APIRoutes started

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,5 +1,36 @@
 //================
 //LOAD DATA
-//We are linking our routes to a series of "Data" sources
-//These data sources hold arrays of information on notes
+//We are linking our routes to a source of db/db.json
+//These data sources hold JSON of information on notes
 //=================
+
+let noteData = require("../db/db.json");
+
+
+//==============
+//Routing
+//==============
+
+module.exports = function(app) {
+    //API GET Requests
+    //Below code handles when users "visit" a page
+    //In each of the below cases when a user visits a link
+    //(ex Localhost:PORT/api/noteData.. they are shown a JSON of the data in the table)
+
+    app.get("/api/notes", function(req, res) {
+        res.json(noteData);
+        console.log(noteData);
+        
+    });
+
+
+    //API POST REQUEST
+    //Below code handles when a user submits a form and thus submits data to the server
+    //In each of the below cases, when a user submits form data (a JSON object)
+
+    app.post("/api/notes", function(req, res) {
+        //The code here will understand if the notes input were null or actual characters. then it will
+    })
+}
+
+
